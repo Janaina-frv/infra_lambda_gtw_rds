b6bba48b9fd9320233f86a93d3fa4ed9f9b1a0cd
Janaina <janainafrv@gmail.com>
2025-12-21 11:22:24 -0300

    Testando destroy

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -86,10 +86,3 @@ resource "aws_db_subnet_group" "this_subnet_group" {
     aws_subnet.private_b
   ]
 }
-
-resource "aws_db_option_group" "this_option_group" {
-  name                     = "option-group"
-  engine_name              = "postgres"
-  major_engine_version     = "17"
-  option_group_description = "Option group importado"
-}
