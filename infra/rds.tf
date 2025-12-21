cd43b4a5b88da704ce5f34ab85fb89c3503e9a06
Janaina <janainafrv@gmail.com>
2025-12-20 21:29:42 -0300

    Primeiras configuracoes

--- infra/rds.tf
+++ infra/rds.tf
@@ -1,44 +1,4 @@
 #####################################
-# Variáveis do RDS
-#####################################
-
-variable "db_name" {
-  description = "Nome do banco de dados"
-  default     = "appdb"
-}
-
-variable "db_username" {
-  description = "Usuário do banco de dados"
-  default     = "admin"
-}
-
-variable "db_password" {
-  description = "Senha do banco de dados"
-  default     = "change_me"
-  sensitive   = true
-}
-
-variable "db_instance_identifier" {
-  description = "Identificador da instância RDS"
-  default     = "items-db"
-}
-
-variable "db_instance_class" {
-  description = "Tipo da instância RDS"
-  default     = "db.t3.micro"
-}
-
-variable "db_allocated_storage" {
-  description = "Armazenamento em GB da instância RDS"
-  default     = 20
-}
-
-variable "db_engine_version" {
-  description = "Versão do Postgres"
-  default     = "15.4"
-}
-
-#####################################
 # Security Group do RDS
 #####################################
 
--- infra/variables.tf
+++ infra/variables.tf
@@ -26,3 +26,23 @@ variable "db_password" {
   description = "Senha do banco"
   sensitive   = true
 }
+
+variable "db_instance_identifier" {
+  description = "Identificador da instância RDS"
+  default     = "items-db"
+}
+
+variable "db_instance_class" {
+  description = "Tipo da instância RDS"
+  default     = "db.t3.micro"
+}
+
+variable "db_allocated_storage" {
+  description = "Armazenamento em GB da instância RDS"
+  default     = 20
+}
+
+variable "db_engine_version" {
+  description = "Versão do Postgres"
+  default     = "15.4"
+}
