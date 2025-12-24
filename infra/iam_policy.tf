40af90f0c6521225943e92e9fd62db28ac3376c2
Janaina <janainafrv@gmail.com>
2025-12-23 21:16:03 -0300

    Testando apply

--- infra/iam_policy.tf
+++ infra/iam_policy.tf
@@ -3,7 +3,7 @@
 #####################################
 
 resource "aws_iam_role" "lambda_exec_role" {
-  name = "lambda-java-exec-role"
+  name = "lambda-java-items-api-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
