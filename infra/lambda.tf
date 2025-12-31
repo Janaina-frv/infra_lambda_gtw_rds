9b3925a194e6e295cbefdd003d0d531827b84081
Janaina <janainafrv@gmail.com>
2025-12-31 17:05:15 -0300

    Testando Destroy

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -7,7 +7,7 @@ resource "aws_lambda_function" "items" {
   role          = aws_iam_role.lambda_exec_role.arn
 
   runtime = var.lambda_runtime
-  filename = "${path.module}/target/function.zip"
+  filename = "${path.module}/build/lambda/function.zip"
   handler  = "io.quarkus.amazon.lambda.runtime.QuarkusStreamHandler::handleRequest"
 
   memory_size = var.lambda_memory
