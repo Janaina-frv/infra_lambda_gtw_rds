b8f0a9af673af40ec9aa67b65b93a58e187695c0
Janaina <janainafrv@gmail.com>
2026-01-07 20:12:28 -0300

    Subindo aplicação na nuvem

--- infra/locals.tf
+++ infra/locals.tf
@@ -3,9 +3,7 @@ locals {
   vpc_id = var.use_existing_vpc ? data.aws_vpc.existing[0].id : aws_vpc.this_vpc[0].id
 
   # Subnets
-  subnet_ids = var.use_existing_vpc
-    ? data.aws_subnets.existing.ids
-    : [aws_subnet.private_a[0].id, aws_subnet.private_b[0].id]
+  subnet_ids = var.use_existing_vpc ? data.aws_subnets.existing.ids : [aws_subnet.private_a[0].id, aws_subnet.private_b[0].id]
 
   # Security Groups
   lambda_sg_id       = var.use_existing_sgs ? data.aws_security_group.lambda_sg[0].id : aws_security_group.lambda_sg[0].id
