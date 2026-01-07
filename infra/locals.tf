e4d329e0231060a2e059fbe6ff3b71634f4b5704
Janaina <janainafrv@gmail.com>
2026-01-07 20:11:22 -0300

    Subindo aplicação na nuvem

--- infra/locals.tf
+++ infra/locals.tf
@@ -4,15 +4,12 @@ locals {
 
   # Subnets
   subnet_ids = var.use_existing_vpc
-    ? data.aws_subnets.existing_private[0].ids
-    : [
-        aws_subnet.private_a[0].id,
-        aws_subnet.private_b[0].id
-      ]
+    ? data.aws_subnets.existing.ids
+    : [aws_subnet.private_a[0].id, aws_subnet.private_b[0].id]
 
   # Security Groups
-  lambda_sg_id        = var.use_existing_sgs ? var.existing_lambda_sg_id : aws_security_group.lambda_sg[0].id
-  rds_sg_id           = var.use_existing_sgs ? var.existing_rds_sg_id : aws_security_group.rds_sg[0].id
-  endpoint_sqs_sg_id  = var.use_existing_sgs ? var.existing_endpoint_sqs_sg_id : aws_security_group.endpoint_sg[0].id
-  endpoint_sns_sg_id  = var.use_existing_sgs ? var.existing_endpoint_sns_sg_id : aws_security_group.endpoint_sns_sg[0].id
+  lambda_sg_id       = var.use_existing_sgs ? data.aws_security_group.lambda_sg[0].id : aws_security_group.lambda_sg[0].id
+  rds_sg_id          = var.use_existing_sgs ? data.aws_security_group.rds_sg[0].id : aws_security_group.rds_sg[0].id
+  endpoint_sqs_sg_id = var.use_existing_sgs ? data.aws_security_group.endpoint_sqs_sg[0].id : aws_security_group.endpoint_sg[0].id
+  endpoint_sns_sg_id = var.use_existing_sgs ? data.aws_security_group.endpoint_sns_sg[0].id : aws_security_group.endpoint_sns_sg[0].id
 }
